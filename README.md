18513d6bd711837cab7ebc9e9fa62b614a35aad8
lele287 <2622552870@qq.com>
2023-03-16 09:06:23 +0800

    项目结构整理

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # 外链演示
-<!-- ```
-npm install 
-``` -->
-## education
-[教育官网](https://lele287.github.io/education)
-## file
-[文件管理](https://lele287.github.io/file)
-## demonstration
-[virtuoso](https://lele287.github.io/demonstration/virtuoso)
-[大眼](https://lele287.github.io/demonstration/eye)
-[水波纹](https://lele287.github.io/demonstration/ripple)
+
+静态网页展示
---
+++ config.js
@@ -0,0 +1,38 @@
+export const items = [
+  {
+    title: 'education',
+    content: '教育官网实例',
+    url: './assets/image/education.png',
+    href: './item/education'
+  },
+  {
+    title: 'file',
+    content: '文件管理页面',
+    url: './assets/image/file.png',
+    href: './item/file'
+  },
+  {
+    title: 'Large eye',
+    content: '大大的眼睛',
+    url: './assets/image/largeEye.png',
+    href: './item/eye'
+  },
+  {
+    title: 'virtuoso',
+    content: '圣诞快乐',
+    url: './assets/image/virtuoso.png',
+    href: './item/virtuoso'
+  },
+  {
+    title: 'water ripple',
+    content: '水波纹',
+    url: './assets/image/waterRipple.png',
+    href: './item/ripple'
+  },
+  {
+    title: 'chart',
+    content: '绘制',
+    url: './assets/image/chart.png',
+    href: './item/chart'
+  }
+]
