cbba458a5f286c5f94659c1a81ab3b3e35d41bf1
lele <2622552870@qq.com>
2023-08-08 18:20:38 +0800

    配置项修改

--- config.js
+++ config.js
@@ -1,6 +1,30 @@
 var CONFIG = {
   items: [
     {
+      title: 'time',
+      content: '时钟翻转',
+      url: './assets/image/time.png',
+      href: './item/time'
+    },
+    {
+      title: 'particle',
+      content: '上落效果',
+      url: './assets/image/particle.png',
+      href: './item/particle'
+    },
+    {
+      title: 'Large eye',
+      content: '大眼特效',
+      url: './assets/image/largeEye.png',
+      href: './item/eye'
+    },
+    {
+      title: 'screen recording',
+      content: '屏幕录制',
+      url: './assets/image/screenRecording.png',
+      href: './item/screenRecording'
+    },
+    {
       title: 'education',
       content: '教育官网实例',
       url: './assets/image/education.png',
@@ -12,18 +36,12 @@ var CONFIG = {
       url: './assets/image/file.png',
       href: './item/file'
     },
-    {
-      title: 'Large eye',
-      content: '大大的眼睛',
-      url: './assets/image/largeEye.png',
-      href: './item/eye'
-    },
-    {
-      title: 'virtuoso',
-      content: '圣诞快乐',
-      url: './assets/image/virtuoso.png',
-      href: './item/virtuoso'
-    },
+    // {
+    //   title: 'virtuoso',
+    //   content: '圣诞快乐',
+    //   url: './assets/image/virtuoso.png',
+    //   href: './item/virtuoso'
+    // },
     {
       title: 'water ripple',
       content: '水波纹',
@@ -43,34 +61,16 @@ var CONFIG = {
       href: './item/logoProduce'
     },
     {
-      title: 'particle',
-      content: '上落效果',
-      url: './assets/image/particle.png',
-      href: './item/particle'
-    },
-    {
-      title: 'screen recording',
-      content: '屏幕录制',
-      url: './assets/image/screenRecording.png',
-      href: './item/screenRecording'
-    },
-    {
       title: 'text animation',
       content: '文本动画效果',
       url: './assets/image/textAnimation.png',
       href: './item/textAnimation'
     },
     {
-      title: 'notelite',
-      content: 'NoteLite',
+      title: 'NoteLite',
+      content: '可粘贴图片的粘贴板',
       url: './assets/image/notelite.png',
       href: './item/notelite'
-    },
-    {
-      title: 'time',
-      content: '时钟翻转',
-      url: './assets/image/time.png',
-      href: './item/time'
     },
     {
       title: 'imgcompress',
